a36aa59942dfc97cd00611ce88636b244e2924ca
Grigory Stukunov <gstukunov@devexperts.com>
2023-06-20 18:46:56 +0300

    firebase configuration

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDlLz6w3rLYJ_n211nRIVCK_tlV2U1GA0A',
-  authDomain: 'handform-c62a3.firebaseapp.com',
-  databaseURL: 'https://handform-c62a3-default-rtdb.firebaseio.com',
-  projectId: 'handform-c62a3',
-  storageBucket: 'handform-c62a3.appspot.com',
-  messagingSenderId: '867248517559',
-  appId: '1:867248517559:web:7eed23797fda6d2ae8f832',
+  apiKey: 'AIzaSyDhTA-weJ8WoR6YO4W9XQL4JJ9kwPl1rx4',
+  authDomain: 'forms-89d10.firebaseapp.com',
+  projectId: 'forms-89d10',
+  storageBucket: 'forms-89d10.appspot.com',
+  messagingSenderId: '792939928640',
+  appId: '1:792939928640:web:086e1fecc60c359ef113a5',
+  measurementId: 'G-8HVVTM2YMC',
 };
 
 const app = initializeApp(firebaseConfig);
